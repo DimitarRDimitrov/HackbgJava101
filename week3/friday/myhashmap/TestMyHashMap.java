49c0fc4fe8e0ae5d59d1827203e12144ef7a6050
DimitarRDimitrov <melodeath93@gmail.com>
2017-03-31 12:12:46 +0300

    Added 2 unit tests for myHashMap

--- week3/friday/myhashmap/TestMyHashMap.java
+++ week3/friday/myhashmap/TestMyHashMap.java
@@ -1,22 +1,42 @@
 package friday.myhashmap;
 
+import org.junit.Test;
+
+import org.junit.Assert;
+
 public class TestMyHashMap {
 
-	public static void main(String[] args) {
-
+//	public static void main(String[] args) {
+//
+//		MyHashMap<String, Integer> mhm = new MyHashMap<>();
+//		
+//		char[] myChars = {'a', 'b', 'c', 'd','e','f','j','h','i','j','k','l','m','o','p','q','r','s'};
+//		for(int i=0; i < 17; i++) {
+//			mhm.put(String.valueOf(myChars[i]), i);
+//		}
+//		mhm.put("h", 333);
+//		
+//		System.out.println(mhm.getCapacity());
+//		System.out.println(mhm.get("h"));
+//		System.out.println(mhm.get("o"));
+//		
+//		
+//	}
+	
+	@Test public void retreiveElement() {
+		MyHashMap<String, String> mhm = new MyHashMap<>();
+		mhm.put("First", "one");
+		Assert.assertEquals("one", mhm.get("First"));
+	}
+	
+	@Test public void resizeMyHashMap() {
 		MyHashMap<String, Integer> mhm = new MyHashMap<>();
 		
 		char[] myChars = {'a', 'b', 'c', 'd','e','f','j','h','i','j','k','l','m','o','p','q','r','s'};
 		for(int i=0; i < 17; i++) {
 			mhm.put(String.valueOf(myChars[i]), i);
 		}
-		mhm.put("h", 333);
-		
-		System.out.println(mhm.getCapacity());
-		System.out.println(mhm.get("h"));
-		System.out.println(mhm.get("o"));
-		
-		
+		Assert.assertEquals(32, mhm.getCapacity());
 	}
 
 }
